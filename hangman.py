f15f95fa7d30bc9b33bd723eea74fdb3427a58bf
Rafael Santos <rafa@blackmagicbox.com.br>
2024-01-28 21:01:37 +0100

    FEAT Curses init screen method to refresh the screan instead of scrolling the terminal session

--- hangman.py
+++ hangman.py
@@ -1,49 +1,67 @@
-def print_lives(strikes):
+import curses
+
+
+def print_lives(stdscr, strikes):
     display = ""
-    print("Strikes left: ")
+    stdscr.addstr("Strikes left: \n")
     for strike in range(strikes):
         display += '|'
-    print(display)
+    stdscr.addstr(display + "\n")
 
 
 def main():
-    print(chr(27) + "[2J")  # clear the screen every round
-    strikes = 10
-    right_guesses = 0
-    word = 'TERMINATOR'
-    guessed_letter = input("Guess a letter: ")
-    display = ['_'] * len(word)
+    stdscr = curses.initscr()
+    curses.noecho()
+    curses.cbreak()
 
-    while guessed_letter != "quit" and strikes > 0:
-        print(chr(27) + "[2J")  # clear the screen every round
-        print("Guesses left: ")
-        print_lives(strikes)
+    try:
+        strikes = 10
+        right_guesses = 0
+        word = 'TERMINATOR'
+        stdscr.addstr("Guess a letter: ")
+        stdscr.refresh()
+        guessed_letter = stdscr.getkey()
 
-        if guessed_letter in word:
-            print("Correct guess")
-            for index, letter in enumerate(word):
-                if letter == guessed_letter:
-                    display[index] = letter
-                    right_guesses += 1
+        display = ['_'] * len(word)
+
+        while guessed_letter != "quit" and strikes > 0:
+            stdscr.clear()
+            stdscr.addstr("Guesses left: \n")
+            print_lives(stdscr, strikes)
+
+            if guessed_letter in word:
+                stdscr.addstr("Correct guess\n")
+                for index, letter in enumerate(word):
+                    if letter == guessed_letter:
+                        display[index] = letter
+                        right_guesses += 1
+            else:
+                strikes -= 1
+                stdscr.addstr("Wrong guess\n")
+            stdscr.addstr(" ".join(display) + "\n")  # Print current state of the word
+            if right_guesses == len(word):
+                break
+            stdscr.addstr("Guess a letter: ")
+            stdscr.refresh()
+            guessed_letter = stdscr.getkey()
+
+        stdscr.clear()
+        stdscr.addstr("Guesses left: \n")
+        print_lives(stdscr, strikes)
+        stdscr.addstr(" ".join(display) + "\n")  # Print current state of the word
+
+        if strikes <= 0:
+            stdscr.addstr("You loose\n")
         else:
-            strikes -= 1
-            print("Wrong guess")
-        print(" ".join(display))  # Print current state of the word
-        if right_guesses == len(word):
-            break
-        guessed_letter = input("Guess a letter: ")
+            stdscr.addstr("Congratulations you won!\n")
 
-    print(chr(27) + "[2J")  # clear the screen every round
-    print("Guesses left: ")
-    print_lives(strikes)
-    print(" ".join(display))  # Print current state of the word
-
-    if strikes <= 0:
-        print("You loose")
-    else:
-        print("Congratulations you won!")
-
-    print("Done!")
+        stdscr.addstr("Press any key to exit...\n")
+        stdscr.refresh()
+        stdscr.getkey()  # Wait for a keypress
+    finally:
+        curses.echo()
+        curses.nocbreak()
+        curses.endwin()
 
 
 main()
